5e5237fcc66a1f10397de45b21c0130ca1664176
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-28 10:40:39 -0300

    ashe

--- App.js
+++ App.js
@@ -1,36 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import MenuList from './MenuList';
+import MenuList from './components/MenuList.jsx';
 //api key 5163b17d295b4d59a4d339fc3b2cbeeb
 const App = () => {
   const [menuData, setMenuData] = useState([]);
-  const [averageHealthscore, setAverageHealthscore] = useState(0);
 
   useEffect(() => {
-    // Aquí deberías hacer una solicitud a la API real para obtener los datos del menú
-    // En lugar de cargar el JSON localmente.
-    // Ejemplo:
-    // fetch('URL_DE_TU_API')
-    //   .then(response => response.json())
-    //   .then(data => setMenuData(data.menu))
-    const data = require('./menuData.json');
-    setMenuData(data.menu);
+    fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=5163b17d295b4d59a4d339fc3b2cbeeb&addRecipeInformation=true',{
+      method:"GET",
+    })
+    .then(response => response.json())
+    .then(response  => setMenuData(response))
+    .catch(err => console.log(err))
   }, []);
-
-  useEffect(() => {
-    const healthscoreSum = menuData.reduce((sum, item) => sum + item.healthscore, 0);
-    setAverageHealthscore(healthscoreSum / menuData.length);
-  }, [menuData]);
-
-  const deleteItem = (id) => {
-    const updatedMenu = menuData.filter((item) => item.id !== id);
-    setMenuData(updatedMenu);
-  };
 
   return (
     <View>
-      <Text>Promedio Healthscore: {averageHealthscore}</Text>
-      <MenuList menuData={menuData} onDelete={deleteItem} />
+      <MenuList menuData={menuData} />
     </View>
   );
 };
--- components/MenuList.jsx
+++ components/MenuList.jsx
@@ -1,35 +1,90 @@
-import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 
-const MenuList = ({ menuData, onDelete }) => {
-  const [totalPrice, setTotalPrice] = useState(0);
+const MenuList = ({ menuData, onPressMoreInfo }) => {
+  const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
 
-  const calculateTotalPrice = () => {
-    const priceSum = menuData.reduce((sum, item) => sum + item.precio, 0);
-    setTotalPrice(priceSum);
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(Dimensions.get('window').width);
+    };
 
-  const deleteItem = (id) => {
-    onDelete(id);
-  };
+    Dimensions.addEventListener('change', handleResize);
+
+    return () => {
+      Dimensions.removeEventListener('change', handleResize);
+    };
+  }, []);
 
   return (
-    <View>
-      <Text>Total Price: {totalPrice}</Text>
+    <View style={styles.container}>
       <FlatList
-        data={menuData}
+        data={menuData.results}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.nombre} - ${item.precio}</Text>
-            <TouchableOpacity onPress={() => deleteItem(item.id)}>
-              <Text>Eliminar</Text>
-            </TouchableOpacity>
+          <View style={[styles.menuItem, { width: screenWidth - 32 }]}>
+            <View style={styles.itemContent}>
+              <Image source={{ uri: item.image }} style={styles.image} />
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text numberOfLines={3} style={styles.summary}>
+                  {item.summary}
+                </Text>
+                <TouchableOpacity onPress={() => onPressMoreInfo(item)}>
+                  <Text style={styles.moreInfoButton}>Más información</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f2f2f2',
+  },
+  menuItem: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  itemContent: {
+    flexDirection: 'row', // Coloca los elementos en una fila
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    marginRight: 8,
+    borderRadius: 8,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  summary: {
+    fontSize: 16,
+    color: '#666',
+  },
+  moreInfoButton: {
+    color: 'blue',
+    marginTop: 8,
+  },
+});
 
 export default MenuList;
