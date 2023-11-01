e389c00d020c17cc3cd6d5b6891dc76abe95ad82
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-01 09:04:37 -0300

    detalles por terminar

--- components/Detalles.jsx
+++ components/Detalles.jsx
@@ -1,65 +1,27 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
-import { StyleSheet } from "react-native";
-import Table from 'react-native-table';
-//no funciona la tabla
+import React from "react";
+import { View, Text, Button, Image } from "react-native";
+import { Table, Row, Cell } from "react-native-table-component";
 
 const Detalles = ({ route, navigation }) => {
   const { plato } = route.params;
-
+ //no se ve la imagen :((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: plato.image.url }} style={styles.image} />
-      <View style={styles.content}>
-        <Text style={styles.title}>{plato.title}</Text>
-        <Text style={styles.summary}>{plato.summary}</Text>
-
-        <Table>
-          <TableRow>
-            <TableCell>Gluten free</TableCell>
-            <TableCell>{plato.glutenFree ? "Sí" : "No"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Health score</TableCell>
-            <TableCell>{plato.healthScore}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Price per serving</TableCell>
-            <TableCell>${plato.pricePerServing}</TableCell>
-          </TableRow>
-        </Table>
+    <View style={{ flex: 1, padding: 16 }}>
+      <Image source={{ uri: plato.image.url }} style={{ width: 200, height: 200 }} />
+      <View style={{ padding: 16 }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>{plato.title}</Text>
+        <Text style={{ fontSize: 16 }}>{plato.summary}</Text>
+        <View style={{ marginTop: 16 }}>
+          <Table>
+            <Row data={["Gluten free", plato.glutenFree ? "Sí" : "No"]} />
+            <Row data={["Health score", plato.healthScore]} />
+            <Row data={["Price per serving", `$${plato.pricePerServing}`]} />
+          </Table>
+        </View>
       </View>
-      <Button title="Volver" onPress={() => navigation.goBack()} />
+      <Button title="Volver" onPress={() => navigation.goBack()} style={{ width: 200, height: 200 }}/>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  image: {
-    width: "100%",
-    height: 200,
-  },
-  content: {
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  summary: {
-    fontSize: 16,
-  },
-  table: {
-    width: "100%",
-    borderCollapse: "collapse",
-  },
-  tableCell: {
-    padding: 8,
-  },
-});
 
 export default Detalles;
